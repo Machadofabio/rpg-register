d027326bf5a76f44a073afbafc9f3b7247e9b9a1
machadofabio <fabio.a.machado@hotmail.com>
2024-01-09 00:19:33 -0300

    feat: first commit

---
+++ src/main/java/br/com/rpgrecordmanagement/rpg/RpgRecordApplication.java
@@ -0,0 +1,13 @@
+package br.com.rpgrecordmanagement.rpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rpgrecordmanagement/rpg/RpgRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rpgrecordmanagement.rpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
